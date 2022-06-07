4897839f4b413de83f1a69623253e23fa7598627
Amares03 <amaressabit0314@gmail.com>
2022-06-07 13:42:36 +0300

    use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-// import logo from "./logo.svg";
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
+      <Navbar dark color="secondary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
